b47ce059545ad17be2b67698dbc4541d676c2b56
VicourtBitt <victorgcb@outlook.com>
2024-09-16 16:57:08 -0300

    Adding some commentaries to the code, and adding gender insertion

--- GENERATOR_TEST/csv_generator.py
+++ GENERATOR_TEST/csv_generator.py
@@ -2,28 +2,27 @@ from names_generator import generate_name
 from random import random, choice
 from datetime import datetime, timedelta
 
-# money = (random() * 1000) + 1
-# state = choice(stateArray)
-# stateArray = [
-#     'RS', 'SC', 'PR', 'SP', 'RJ', 'MG', 'ES', 'MT', 'MS',
-#     'GO', 'DF', 'BA', 'SE', 'AL', 'PE', 'PB', 'RN', 'CE',
-#     'PI', 'MA', 'PA', 'AP', 'TO', 'RO', 'AC', 'AM', 'RR'
-# ]
-
+# Função para gerar datas aleatórias  
 def gen_datetime(min_year=2020, max_year=datetime.now().year):
     start = datetime(min_year, 1, 1, 00, 00, 00)
     years = max_year - min_year + 1
     end = start + timedelta(days=365 * years)
     return (start + (end - start) * random()).strftime("%d-%m-%Y")
 
+# Função para criar um arquivo CSV com dados de teste
 def create_csv(num: int):
+
+    # Abre o arquivo data_sample.csv em modo de escrita
     with open('data_sample.csv', 'w') as file:
 
+        # Escreve o cabeçalho do arquivo
         file.write('id,name,paid,isFiliated,state,date\n')
 
+        # Loop para gerar os dados de teste baseado no número passado como argumento
         for i in range(num):
-            userId = i
+            userId = i + 1
             name = generate_name(style='capital')
+            gender = choice(["Masculino", "Feminino", "Não-binário"])
             paid = ((random() * 1000) + 1)
             isFiliated = choice([True, False])
             state = choice([
@@ -32,6 +31,7 @@ def create_csv(num: int):
                 'PI', 'MA', 'PA', 'AP', 'TO', 'RO', 'AC', 'AM', 'RR'
             ])
             date = gen_datetime()
-            file.write(f'{userId},{name},{paid:.2f},{isFiliated},{state},{date}\n')
+            # Escreve os dados em cada linha do arquivo
+            file.write(f'{userId},{name},{gender},{paid:.2f},{isFiliated},{state},{date}\n')
 
 create_csv(1000)
--- GENERATOR_TEST/main.py
+++ GENERATOR_TEST/main.py
@@ -1,12 +1,18 @@
 import pandas as pd
 
+# O caminho do arquivo armazenado em um CSV
 csv_path = "pandas_python//data_sample.csv"
 
+# Lê o arquivo CSV e armazena em um DataFrame
 df = pd.read_csv(csv_path, index_col='id')
 
+# Filtra os dados para encontrar os pagantes que pagaram mais de R$ 800,00
 big_payers = df.loc[df['paid'] > 800.00]
 
+# Filtra os dados para encontrar os pagantes que são filiados
 filiatedBigPlayers = big_payers.loc[big_payers['isFiliated'] == True]
 
-fbpRS = filiatedBigPlayers.loc[filiatedBigPlayers['state'] == 'AM']
-print(fbpRS)
+# Filtra os dados para encontrar os pagantes que são filiados e são do estado do Amazonas
+fbpAM = filiatedBigPlayers.loc[filiatedBigPlayers['state'] == 'AM']
+
+print(fbpAM)
